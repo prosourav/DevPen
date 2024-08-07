8734870c3ed6a119ac5341aa0f18d3c42874a87c
Sourav Saha <dev.souravsaha@gmail.com>
2024-08-07 10:53:22 +0530

    Debug-  .env

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ import 'devicon/devicon.min.css';
 
 function App() {
 
+
+  const key = import.meta.env.VITE_API_KEY;
+  const path = import.meta.env.VITE_BASE_PATH;
+  const url = import.meta.env.VITE_BASE_URL;
+  const host = import.meta.env.VITE_BASE_HOST;
+
+
+  console.log('key', key);
+  console.log('path', path);
+  console.log('url', url);
+  console.log('host', host);
+
   return (
     <PlayGroundProvider>
       <DirectoryInfoProvider>
